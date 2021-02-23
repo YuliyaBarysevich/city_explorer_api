6a255c10be0095f9ff3c03315e024d809e191683
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-02-22 16:57:21 -0800

    created local host

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 'use strict';
 
+// Packages
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+//express() will return server object
+const app = express(); 
+// enables local processes
+app.use(cors());
+
+const PORT = process.env.PORT || 3009;
+
+// Routes
+// app.get('/location')
+// function locationCallback(req, res){
+//   res.send()
+// }
+
+app.listen(PORT, () => console.log(`app is up on port http://localhost:${PORT}`))
